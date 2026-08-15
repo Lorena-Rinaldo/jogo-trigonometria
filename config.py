9107b02ad09e8abb39ec0889383ca4399ed7acf0
Lorena Rinaldo <lorena_edu@outlook.com>
2026-08-15 11:17:46 -0300

    Feat: Configurações globais

--- config.py
+++ config.py
@@ -1,1 +1,19 @@
-# Guarda as configurações globais da aplicação, como ambiente, debug e chaves.
+"""Guarda as configurações globais da aplicação."""
+
+
+class Config:
+    """Configurações principais do backend."""
+
+    DEBUG = True
+    SECRET_KEY = "jogo-trigonometria-dev"
+    JSON_SORT_KEYS = False
+
+    SWAGGER_TEMPLATE = {
+        "swagger": "2.0",
+        "info": {
+            "title": "API - Jogo de Trigonometria",
+            "description": "Backend para jogo educativo de funções trigonométricas.",
+            "version": "1.0.0",
+        },
+        "basePath": "/",
+    }
